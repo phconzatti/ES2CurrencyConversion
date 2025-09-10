b9d52ad168d6b96644b16d8e015667ea08c7a4f4
Pedro Henrique Schneider Conzatti <phconzatti@hotmail.com>
2025-09-10 12:43:33 -0300

    .

--- src/main/java/com/example/demo/InformationCollectorProxy.java
+++ src/main/java/com/example/demo/InformationCollectorProxy.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "information-service", url = "http://localhost:8082")
+@FeignClient(name = "information-collector", url = "http://localhost:8100")
 public interface InformationCollectorProxy {
     @PostMapping("/historico")
     void salvar(@RequestBody CurrencyConversion conversion);
